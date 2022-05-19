d8bca27943f718274ab3952ef926c64f6cc45ce2
Jesper Dalberg <jdnln@yousee.dk>
2022-05-19 09:34:58 +0200

    Added some hyper stuff to serve a connection request

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
-use std::fmt;
+use error::BoxError;
+use log::debug;
+use std::convert::Infallible;
 use std::sync::Arc;
+use tokio::sync::mpsc::Sender;
 
 #[macro_use]
 mod error;
@@ -7,6 +10,11 @@ mod messages;
 
 use error::Error;
 use error::Result;
+use hyper::{
+    header::CONTENT_TYPE,
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server,
+};
 use tokio::sync::mpsc;
 use tokio::sync::mpsc::Receiver;
 
@@ -57,6 +65,26 @@ struct Config {
     conn_req_port: u16,
 }
 
+async fn serve_connection_request(
+    req: Request<Body>,
+    response_channel_tx: Sender<u32>,
+) -> core::result::Result<Response<Body>, BoxError> {
+    let response = if response_channel_tx.send(0).await.is_err() {
+        Response::builder()
+            .status(500)
+            .header(CONTENT_TYPE, "text/html")
+            .body(Body::from(""))
+            .unwrap()
+    } else {
+        Response::builder()
+            .status(200)
+            .header(CONTENT_TYPE, "text/html")
+            .body(Body::from(""))
+            .unwrap()
+    };
+    Ok(response)
+}
+
 impl ClientBuilder {
     /// Constructs a new `ClientBuilder`.
     ///
@@ -93,6 +121,7 @@ impl ClientBuilder {
 
     /// process all the message types.
     async fn process_messages(
+        self,
         mut periodic_rx: Receiver<u32>,
         mut connection_request_rx: Receiver<u32>,
         mut notification_change_rx: Receiver<String>,
@@ -103,7 +132,6 @@ impl ClientBuilder {
             }
             connection_request = connection_request_rx.recv() => {
                 dbg!(connection_request);
-
             }
             notification_param_changed = notification_change_rx.recv() => {
                 dbg!(notification_param_changed);
@@ -119,10 +147,29 @@ impl ClientBuilder {
     /// The message processor
     ///
     /// Once the tasks are started, send the initial INFORM
-    async fn run() {
+    async fn run(self) {
         let (periodic_tx, periodic_rx) = mpsc::channel::<u32>(100);
+        let (connection_request_tx, connection_request_rx) = mpsc::channel::<u32>(100);
 
         // if periodic informs are enabled, start a timer to handle it
+
+        // create the hyper task
+        let port = self.config.conn_req_port;
+        let addr = ([127, 0, 0, 1], port).into();
+        debug!("Listening for Connection Requests on http://{}", addr);
+        let make_service = make_service_fn(move |_conn| {
+            let cr_tx = connection_request_tx.clone();
+
+            async move {
+                Ok::<_, Infallible>(service_fn(move |req: Request<Body>| {
+                    let cr_tx = cr_tx.clone();
+
+                    serve_connection_request(req, cr_tx)
+                }))
+            }
+        });
+
+        let hyper_task = Server::bind(&addr).serve(make_service);
     }
 }
 
